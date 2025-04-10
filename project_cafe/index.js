7b20e5be1a291a94f90c3a4e5a1962061d03624f
unknown <fikrialhaidar17@gmail.com>
2025-04-10 20:36:52 +0700

    Hello world

---
+++ project_cafe/index.js
@@ -0,0 +1,12 @@
+console.log("Hello, saya Alhaidar"); {
+}
+
+let totalbelanja = 15000;
+
+if (totalbelanja > 500000) {
+      console.log("selamat anda mendapatkan tiket kejepang!);
+       } else if > 10000;  {
+
+       } else {
+        console.log("Terimakasih sudah berbelanja!");
+       }
